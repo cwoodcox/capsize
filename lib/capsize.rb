3b2720f09ec88352918fd51b1aac2d1e8d745383
Corey Woodcox <corey.woodcox@gmail.com>
2011-12-20 18:32:48 -0700

    stuff that works works

--- lib/capsize.rb
+++ lib/capsize.rb
@@ -1,5 +1,39 @@
 require "capsize/version"
 
 module Capsize
-  # Your code goes here...
+  extend ActiveSupport::Concern
+  DOWNCASE_FIELDS = []
+  TITLEIZE_FIELDS = []
+
+  module ClassMethods
+    def downcase(*args)
+      set_callback :save, :before, :downcase!
+      options = args.extract_options!
+    
+      args.each do |field|
+        Capsize::DOWNCASE_FIELDS << field
+      end
+    end
+
+    def titleize(*args)
+      set_callback :save, :before, :titleize!
+      options = args.extract_options!
+
+      args.each do |field|
+        Capsize::TITLEIZE_FIELDS << field
+      end
+    end
+  end
+
+  def downcase!
+    DOWNCASE_FIELDS.each do |field|
+      self.send("#{field}=", self.send(field).downcase)
+    end
+  end
+
+  def titleize!
+    TITLEIZE_FIELDS.each do |field|
+      self.send("#{field}=", self.send(field).titleize)
+    end
+  end
 end
